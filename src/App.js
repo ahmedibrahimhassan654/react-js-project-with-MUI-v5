1a2cad9ccacf29962916e260b6bd27ea658a447d
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-06-14 18:38:06 +0200

    MUI layout (grid,container,box)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Feed from "./components/Feed";
+import Sidebar from "./components/Sidebar";
+import Rightbar from "./components/Rightbar";
+import { Box, Stack } from "@mui/material";
 function App() {
-  return <div></div>;
+  return (
+    <Box>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        {/* <Navbar/> */}
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
+  );
 }
 
 export default App;
